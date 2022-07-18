2df052736741ea0facb36dd4c9abf3f95ac37abc
JuanJoseMora <juanmora.dev@gmail.com>
2022-07-18 12:08:40 +0200

    First commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new Symfony 5 application!'
+        ]);
+    }
+}
